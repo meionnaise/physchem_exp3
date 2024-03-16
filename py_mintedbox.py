78949ad2dc200f555bd8b6ccbded912712791dc7
meionnaise <69590103+meionnaise@users.noreply.github.com>
2024-03-16 20:19:41 +1030

    Add files via upload

---
+++ py_mintedbox.py
@@ -0,0 +1,13 @@
+def write_python_file(filename):
+    with open(filename) as f:
+        data = f.read()
+        f.close()
+
+    new_data = "\\begin{mintedbox}{python}\n\t" + data + "\n\end{mintedbox}"
+    new_filename = filename[:-2]+ "txt"
+
+    with open(new_filename, mode="w") as f:
+        f.write(new_data)
+        f.close()
+
+write_python_file("q1.py")
